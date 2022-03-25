32d9426ea81cd1187b740a79445eb357e1b12d9f
SantiagoSHF <102351257+SantiagoSHF@users.noreply.github.com>
2022-03-25 09:11:26 +0100

    Delete Ejercicios directory

--- Ejercicios/scripts/script_ejercicio_1.js
+++
@@ -1,8 +0,0 @@
-//ejercicio_1
-var precio, cantidad, pagar;
-
-precio=parseFloat(prompt("Introduzca el precio del producto"));
-cantidad=parseInt(prompt("Introduzca la cantidad que desea comprar"));
-pagar=precio*cantidad;
-
-document.write("Deberá pagar " + pagar + "€ por la cantidad de " + cantidad + " unidades del producto");
--- Ejercicios/scripts/script_ejercicio_19.js
+++
@@ -1,3 +0,0 @@
-function dados(){
-    let dinero = parseFloat
-}
